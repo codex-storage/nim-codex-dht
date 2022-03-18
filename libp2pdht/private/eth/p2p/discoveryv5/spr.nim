caa301f5abdc4ac6e7adeba730ef477d0360280c
Eric Mastro <eric.mastro@gmail.com>
2022-03-18 15:23:35 +1100

    Allow multiaddress to be updated

--- libp2pdht/private/eth/p2p/discoveryv5/spr.nim
+++ libp2pdht/private/eth/p2p/discoveryv5/spr.nim
@@ -6,8 +6,8 @@
 #
 import
   chronicles,
-  std/[options, strutils, sugar],
-  pkg/stew/[results, byteutils],
+  std/[options, sequtils, strutils, sugar],
+  pkg/stew/[results, byteutils, arrayops],
   stew/endians2,
   stew/shims/net,
   stew/base64,
@@ -129,12 +129,78 @@ proc update*(r: var SignedPeerRecord, pk: crypto.PrivateKey,
   if pubkey.isNone() or keysPubKey != keysPrivKey.toPublicKey:
     return err("Public key does not correspond with given private key")
 
-  let updated = if r.data.addresses.len == 0:
-                  MultiAddress.init()
-                else: r.data.addresses[0].address
-  # TODO: Update MultiAddress details here
-  if true: # only if we actually updated the SignedPeerRecord
-    ? r.incSeqNo(keysPrivKey)
+  var
+    changed = false
+    transProto = IpTransportProtocol.udpProtocol
+    transProtoPort: Port
+
+  var updated: MultiAddress
+
+  if r.data.addresses.len == 0:
+    changed = true
+    if ip.isNone:
+      return err "No existing address in SignedPeerRecord with no IP provided"
+
+    if udpPort.isNone and tcpPort.isNone:
+      return err "No existing address in SignedPeerRecord with no port provided"
+
+    let ipAddr = try: ValidIpAddress.init(ip.get)
+                 except ValueError as e:
+                   return err ("Existing address contains invalid address: " & $e.msg).cstring
+    if tcpPort.isSome:
+      transProto = IpTransportProtocol.tcpProtocol
+      transProtoPort = tcpPort.get
+    if udpPort.isSome:
+      transProto = IpTransportProtocol.udpProtocol
+      transProtoPort = udpPort.get
+
+    updated = MultiAddress.init(ipAddr, transProto, transProtoPort)
+
+  else:
+    let
+      existing = r.data.addresses[0].address
+      existingNetProto = ? existing[0].mapErr((e: string) => e.cstring)
+      existingTransProto = ? existing[1].mapErr((e: string) => e.cstring)
+      existingNetProtoFam = ? existingNetProto.protoCode
+                               .mapErr((e: string) => e.cstring)
+      existingNetProtoAddr = ? existingNetProto.protoAddress
+                               .mapErr((e: string) => e.cstring)
+      existingTransProtoCodec = ? existingTransProto.protoCode
+                              .mapErr((e: string) => e.cstring)
+      existingTransProtoPort = ? existingTransProto.protoAddress
+                                 .mapErr((e: string) => e.cstring)
+      existingIp =
+        if existingNetProtoFam == MultiCodec.codec("ip6"):
+          ipv6 array[16, byte].initCopyFrom(existingNetProtoAddr)
+        else:
+          ipv4 array[4, byte].initCopyFrom(existingNetProtoAddr)
+
+      ipAddr = ip.get(existingIp)
+
+
+    if tcpPort.isNone and udpPort.isNone:
+      transProto =
+        if existingTransProtoCodec == MultiCodec.codec("udp"):
+            IpTransportProtocol.udpProtocol
+        else: IpTransportProtocol.tcpProtocol
+      transProtoPort = Port(uint16.fromBytesBE(existingTransProtoPort))
+
+    else:
+      if tcpPort.isSome:
+        transProto = IpTransportProtocol.tcpProtocol
+        transProtoPort = tcpPort.get
+      if udpPort.isSome:
+        transProto = IpTransportProtocol.udpProtocol
+        transProtoPort = udpPort.get
+
+    updated = MultiAddress.init(ipAddr, transProto, transProtoPort)
+    changed = existing != updated
+
+  r.data.addresses[0].address = updated
+
+  # increase the sequence number only if we've updated the multiaddress
+  if changed: ? r.incSeqNo(keysPrivKey)
+
   r = ? SignedPeerRecord.init(pk, r.data)
           .mapErr((e: CryptoError) => ("Failed to update SignedPeerRecord: " & $e).cstring)
 
@@ -217,18 +283,55 @@ proc init*(T: type SignedPeerRecord, seqNum: uint64,
   ## Can fail in case the record exceeds the `maxSprSize`.
 
   let peerId = PeerId.init(pk).get
-  var ma:MultiAddress
-  if ip.isSome and udpPort.isSome:
-    # let ta = initTAddress(ip.get, udpPort.get)
-    # echo ta
-    # ma = MultiAddress.init(ta).get
-    #let ma1 = MultiAddress.init("/ip4/127.0.0.1").get() #TODO
-    #let ma2 = MultiAddress.init(multiCodec("udp"), udpPort.get.int).get
-    #ma = ma1 & ma2
-    ma = MultiAddress.init("/ip4/127.0.0.1/udp/" & $udpPort.get.int).get #TODO
+
+  if tcpPort.isSome() and udpPort.isSome:
+    warn "Both tcp and udp ports specified, using udp in multiaddress",
+      tcpPort, udpPort
+
+  var
+    ipAddr = try: ValidIpAddress.init("127.0.0.1")
+             except ValueError as e:
+               return err ("Existing address contains invalid address: " & $e.msg).cstring
+    proto: IpTransportProtocol
+    protoPort: Port
+
+  if ip.isSome():
+
+    ipAddr = ip.get
+
+    if tcpPort.isSome():
+      proto = IpTransportProtocol.tcpProtocol
+      protoPort = tcpPort.get()
+    if udpPort.isSome():
+      proto = IpTransportProtocol.udpProtocol
+      protoPort = udpPort.get()
   else:
-    ma = MultiAddress.init()
-    # echo "not implemented"
+    if tcpPort.isSome():
+      proto = IpTransportProtocol.tcpProtocol
+      protoPort = tcpPort.get()
+    if udpPort.isSome():
+      proto = IpTransportProtocol.udpProtocol
+      protoPort = udpPort.get()
+
+
+  let ma = MultiAddress.init(ipAddr, proto, protoPort)
+  # if ip.isSome:
+  #   let
+  #     ipAddr = ip.get
+  #     proto = ipAddr.family
+  #     address = if proto == IPv4: ipAddr.address_v4
+  #               else: ipAddr.address_v6
+  #   u and udpPort.isSome
+  #   # let ta = initTAddress(ip.get, udpPort.get)
+  #   # echo ta
+  #   # ma = MultiAddress.init(ta).get
+  #   #let ma1 = MultiAddress.init("/ip4/127.0.0.1").get() #TODO
+  #   #let ma2 = MultiAddress.init(multiCodec("udp"), udpPort.get.int).get
+  #   #ma = ma1 & ma2
+  #   ma = MultiAddress.init("/ip4/127.0.0.1/udp/" & $udpPort.get.int).get #TODO
+  # else:
+  #   ma = MultiAddress.init()
+  #   # echo "not implemented"
 
   let pr = PeerRecord.init(peerId, @[ma], seqNum)
   SignedPeerRecord.init(pk, pr).mapErr((e: CryptoError) => ("Failed to init SignedPeerRecord: " & $e).cstring)
@@ -249,6 +352,7 @@ proc contains*(r: SignedPeerRecord, fp: (string, seq[byte])): bool =
   #   if field.get() == fp[1]:
   #     return true
   # TODO: Implement if SignedPeerRecord custom field pairs are implemented
+  debugEcho "`contains` is not yet implemented for SignedPeerRecords"
   return false
 
 proc `==`*(a, b: SignedPeerRecord): bool = a.data == b.data
