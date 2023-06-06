0970f3e4e7329c95e9ca066b285988255589ba5d
Csaba Kiraly <csaba.kiraly@gmail.com>
2023-06-06 11:05:09 +0200

    add DHT storage (addValue/getValue) functionality
    this is a minimal implementation, with lots of
    work still needed.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

---
+++ libp2pdht/dht/value_messages.nim
@@ -0,0 +1,14 @@
+import
+  ../discv5/[node]
+
+type
+  AddValueMessage* = object
+    cId*: NodeId
+    value*: seq[byte]
+
+  GetValueMessage* = object
+    cId*: NodeId
+
+  ValueMessage* = object
+    #total*: uint32
+    value*: seq[byte]
