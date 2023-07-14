9a78c70bb1cfdcd0c46fa88ad7c0470efa4dc4df
Jaremy Creechley <creechley@gmail.com>
2023-07-13 22:21:25 -0700

    cleanup

--- tests/testAll.nim
+++ tests/testAll.nim
@@ -1,10 +1,6 @@
-when defined(testsPart1) or not defined(testParts):
-  import ./dht/test_providers
-when defined(testsPart2) or not defined(testParts):
-  import ./dht/test_providermngr
-when defined(testsPart3) or not defined(testParts):
-  import ./discv5/test_discoveryv5
-when defined(testsPart4) or not defined(testParts):
-  import ./discv5/test_discoveryv5_encoding
+import ./dht/test_providers
+import ./dht/test_providermngr
+import ./discv5/test_discoveryv5
+import ./discv5/test_discoveryv5_encoding
 
 {.warning[UnusedImport]: off.}
