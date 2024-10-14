fee5a9ced29022adcac9dd9d914439b3818a5bc3
Csaba Kiraly <csaba.kiraly@gmail.com>
2024-10-14 10:11:37 +0200

    set NoreplyRemoveThreshold  to 0.5
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- codexdht/private/eth/p2p/discoveryv5/protocol.nim
+++ codexdht/private/eth/p2p/discoveryv5/protocol.nim
@@ -136,7 +136,7 @@ const
   FindnodeSeenThreshold = 1.0 ## threshold used as findnode response filter
   LookupSeenThreshold = 0.0 ## threshold used for lookup nodeset selection
   QuerySeenThreshold = 0.0 ## threshold used for query nodeset selection
-  NoreplyRemoveThreshold = 0.0 ## remove node on no reply if 'seen' is below this value
+  NoreplyRemoveThreshold = 0.5 ## remove node on no reply if 'seen' is below this value
 
 func shortLog*(record: SignedPeerRecord): string =
   ## Returns compact string representation of ``SignedPeerRecord``.
